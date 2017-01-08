4805e871ba200cb72b49dc73e2d3515ccf3be70b
Federico D'Ambrosio <fedexist@gmail.com>
2017-01-08 16:37:27 +0100

    Add enemy orc spritesheet.

--- SDLOpenGL/GameClass.cpp
+++ SDLOpenGL/GameClass.cpp
@@ -111,9 +111,10 @@ void GameClass::loadMedia()
 	allTextures.push_back(LTexture2D("./assets/CampFireFinished.png",64,64,60));
 	allTextures.push_back(LTexture2D("./assets/player.png", 64, 64, 60));
 	allTextures.push_back(LTexture2D("./assets/life.png", 64, 64,60));
+	allTextures.push_back(LTexture2D("./assets/orc.png", 64, 64, 60));
 
 	allObjectsFactory.push_back(new Fire(glm::vec2(0.0, 0.0), glm::vec2(0, 0), glm::vec2(64, 64), true, true, &allTextures.at(0), 0.05, 0, 4));
-	allObjectsFactory.push_back(new Player(glm::vec2(4.5, 4.5), glm::vec2(0.0, 0.0), glm::vec2(64, 64), true, true, &allTextures.at(1), 1, 26, 28));
+	allObjectsFactory.push_back(new Player(glm::vec2(0.0, 0.0), glm::vec2(0.0, 0.0), glm::vec2(64, 64), true, true, &allTextures.at(3), 1, 26, 28));
 	allObjectsFactory.push_back(new HealthBar(glm::vec2(0.0, 0.0), glm::vec2(0, 0), glm::vec2(64, 64), true, true, &allTextures.at(2), 0.05, 0, 4));
 
 	player_ = new Player(glm::vec2(4.5, 4.5), glm::vec2(0.0, 0.0), glm::vec2(64, 64), true, true, &allTextures.at(1), 1, 26, 28);
