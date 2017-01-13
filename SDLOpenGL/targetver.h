9b640051f2a41441ab47a994f978c8a10c80b8d9
Federico D'Ambrosio <fedexist@gmail.com>
2017-01-13 22:08:31 +0100

    Add (commented) lines for targeting Windows 7. For a successful compiling, 8.1 SDK should be used from project configuration.

--- SDLOpenGL/targetver.h
+++ SDLOpenGL/targetver.h
@@ -5,4 +5,14 @@
 // Se si desidera compilare l'applicazione per una piattaforma Windows precedente, includere WinSDKVer.h e
 // impostare la macro _WIN32_WINNT sulla piattaforma da supportare prima di includere SDKDDKVer.h.
 
+
+//#include <WinSDKVer.h>
+
+/*
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0601 
+#endif
+*/
+
 #include <SDKDDKVer.h>
+
